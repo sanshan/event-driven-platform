0932079cbc35f4e2829e5fd9335f466e18233553
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-20 22:51:23 +0200

    refactor(reader): bind Reader directly to Query contract

--- packages/reader/src/lib/reader.ts
+++ packages/reader/src/lib/reader.ts
@@ -1,5 +1,6 @@
-import type { AnyQuery, QueryResultOf } from '@event-driven-platform/query';
+import type { Query } from '@event-driven-platform/query';
+import type { AnyRead, ReadResultOf } from '@event-driven-platform/read';
 
 export interface Reader {
-    execute<TQuery extends AnyQuery>(query: TQuery): Promise<QueryResultOf<TQuery>>;
+    execute<TRead extends AnyRead>(query: Query<TRead>): Promise<ReadResultOf<TRead>>;
 }
