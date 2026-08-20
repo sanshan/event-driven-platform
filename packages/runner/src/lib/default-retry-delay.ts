cf12eca1a4e774d80b5d397faf0eb0e0d150929e
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-20 13:13:00 +0200

    refactor(runner): remove old retry delay implementation location

--- packages/runner/src/lib/default-retry-delay.ts
+++
@@ -1,9 +0,0 @@
-import type { RetryDelay } from './retry-delay.js';
-
-export class DefaultRetryDelay implements RetryDelay {
-    async wait(delayMs: number): Promise<void> {
-        await new Promise<void>((resolve) => {
-            setTimeout(resolve, delayMs);
-        });
-    }
-}
