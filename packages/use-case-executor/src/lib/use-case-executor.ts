cc3d575ebb57e08aa8cd4b3a632a25b7eb4fa578
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-22 07:24:55 +0200

    feat(use-case-executor): define public executor contract

--- packages/use-case-executor/src/lib/use-case-executor.ts
+++ packages/use-case-executor/src/lib/use-case-executor.ts
@@ -1,3 +1,5 @@
-export function useCaseExecutor(): string {
-    return 'use-case-executor';
+import type { UseCaseExecutionRequest } from './use-case-execution-request.js';
+
+export interface UseCaseExecutor {
+    execute<TInput, TResult>(request: UseCaseExecutionRequest<TInput, TResult>): Promise<TResult>;
 }
