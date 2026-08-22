5754b00cc5d409918d6dc25d1575179ee496fa8e
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-22 08:29:37 +0200

    refactor(use-case): mark timer dependency internal

--- packages/use-case-executor/src/lib/use-case-executor-dependencies.ts
+++ packages/use-case-executor/src/lib/use-case-executor-dependencies.ts
@@ -2,8 +2,13 @@ import type { Clock } from '@event-driven-platform/clock';
 import type { ExecutionIdFactory } from '@event-driven-platform/execution';
 import type { UseCaseExecutionStore } from '@event-driven-platform/use-case-execution-store';
 
+import type { UseCaseExecutorTimer } from './use-case-executor-timer.js';
+
 export interface UseCaseExecutorDependencies {
     readonly clock: Clock;
     readonly executionIdFactory: ExecutionIdFactory;
     readonly store: UseCaseExecutionStore;
+
+    /** @internal */
+    readonly timer?: UseCaseExecutorTimer;
 }
