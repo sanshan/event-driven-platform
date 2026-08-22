678722917cf1194157d735723667b0f7cf177994
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-22 09:26:23 +0200

    refactor(use-case-executor): remove old flat executor contract (#124)

--- packages/use-case-executor/src/lib/use-case-executor.ts
+++
@@ -1,5 +0,0 @@
-import type { UseCaseExecutionRequest } from './use-case-execution-request.js';
-
-export interface UseCaseExecutor {
-    execute<TInput, TResult>(request: UseCaseExecutionRequest<TInput, TResult>): Promise<TResult>;
-}
