88a05c3ef85bd6acf4d6e69962dedc4a857b43e6
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-25 08:15:15 +0200

    feat(use-case-executor): preserve context type

--- packages/use-case-executor/src/lib/executor/use-case-executor.ts
+++ packages/use-case-executor/src/lib/executor/use-case-executor.ts
@@ -1,5 +1,9 @@
+import type { UseCaseContext } from '@event-driven-platform/use-case';
+
 import type { UseCaseExecutionRequest } from './use-case-execution-request.js';
 
 export interface UseCaseExecutor {
-    execute<TInput, TResult>(request: UseCaseExecutionRequest<TInput, TResult>): Promise<TResult>;
+    execute<TInput, TResult, TContext extends UseCaseContext = UseCaseContext>(
+        request: UseCaseExecutionRequest<TInput, TResult, TContext>,
+    ): Promise<TResult>;
 }
