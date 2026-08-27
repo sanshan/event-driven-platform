a60ffb94876521bf272d5d06e8bc95f764affd59
Aleksandr Lihih <sanshan@users.noreply.github.com>
2026-08-27 23:09:01 +0200

    feat(observability): add reader observer

---
+++ packages/observability/src/lib/reader/reader-observer.ts
@@ -0,0 +1,4 @@
+import type { Observer } from '../observer/observer.js';
+import type { ReaderObservation } from './reader-observation.js';
+
+export type ReaderObserver = Observer<ReaderObservation>;
